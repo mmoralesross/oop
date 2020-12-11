2d5e399f54c12e20f51e323ddaef0fc0cd97c080
“mmoralesross” <mmoralesross@gmail.com>
2020-12-11 09:43:22 -0500

    finishing employee part, console logs and returns

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,37 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email, role) {
+    this.id = id;
+    this.name = name;
+    this.role = role;
+    this.email = email;
+}
+
+getName() {
+    console.log(this.name);
+    return `${this.name}`;
+}
+
+getID() {
+    console.log(this.id);
+    return `${this.name}`;
+}
+
+getEmail() {
+    console.log(this.email);
+    return `${this.email}`;
+}
+
+getRole() {
+    console.log ("Employee");
+    return `Employee`;
+}
+
+printInfo() {
+    console.log(`This Employee has ${this.role}`)
+}
+
+}
+
+module.exports = Employee;
+
